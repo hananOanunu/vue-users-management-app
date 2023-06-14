a4769dfbb3b9db6e5c44ebbc8b28808213ffe7bb
hananOanunu <40333166+hananOanunu@users.noreply.github.com>
2023-06-14 09:41:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-users-management-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-xsnrwi)
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+   <!-- 
+Your challenge should you choose to accept it:
+   1. Fetch a list of users from https://jsonplaceholder.typicode.com/users.
+   2. Display the users ID, Name and Email, as a table, where each raw represent a single user
+   3. The users should be displayed ordered by ID.
+   4. It should be possible to delete a user.
+   5. It should be possible to reorder the users, by dragging and dropping a user to the appropriate position. When a user position changes, the id of the user should be updated as well to reflect the current position. For example, dragging user #5 to the first position, will update this user ID to be #1, the original first user  ID will become #2, the original second user Id will become #3 and so on.
+ -->
+
+<template>
+  <div id="app">
+  <!-- Implement Me -->
+  </div>
+</template>
+
+<script>
+export default {
+// <!-- Implement Me -->
+}
+</script>
+
+<style>
+#app {
+// <!-- Implement Me -->
+}
+</style>
