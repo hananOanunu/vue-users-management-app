bdd2f3aeb09a25deac5da557b585a0acbf614e4d
hananOanunu <40333166+hananOanunu@users.noreply.github.com>
2023-06-18 12:54:38 +0300

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
    <!-- 
-   Your mission, should you choose to accept it:
+   Your challenge should you choose to accept it:
    1. Fetch a list of users from https://jsonplaceholder.typicode.com/users.
    2. Display the users ID, Name and Email, as a table, where each raw represent a single user
-   3. The users should be displayed ordered by ID.
-   4. Once a raw was clicked, the user's name should be striked and pained in red.
+   3. Once a raw was clicked, the user's name should be striked and pained in red.
 
    Bonus: 
-   It should be possible to reorder the users, by dragging and dropping a user to the appropriate position. When a user position changes, the id of the user should be updated as well to reflect the current position. For example, dragging user #5 to the first position, will update this user ID to be #1, the original first user  ID will become #2, the original second user Id will become #3 and so on.
+   It should be possible to reorder the users, by dragging and dropping a user to the appropriate position.
  -->
 
 <template>
